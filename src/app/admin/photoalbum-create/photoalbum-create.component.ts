8fc30440c8b8c9f8d5403f7b2c3dfc289bd04c69
tariq <tariqabdelghani@gmail.com>
2020-01-04 14:44:10 +0200

    adding photolbum create

--- src/app/admin/photoalbum-create/photoalbum-create.component.ts
+++ src/app/admin/photoalbum-create/photoalbum-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudOperationsService } from '../services/crud-operations.service';
 
 @Component({
   selector: 'app-photoalbum-create',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotoalbumCreateComponent implements OnInit {
 
-  constructor() { }
+  baseURL = "http://localhost:3000/api/photoalbums";
+  deaprtmentsURL = "http://localhost:3000/api/departments";
+
+  departments: any = [
+    { _id: 0, nameEn: "department1" }
+  ];
+  constructor(private crud: CrudOperationsService) { }
 
   ngOnInit() {
+    this.getDepartments();
+  }
+
+  createSubmit(fCreate) {
+    //console.log(fCreate.value);
+    //console.log(fCreate);
+    const fd = new FormData(fCreate);
+    this.createPhotoalbum(fd);
+  }
+
+  getDepartments() {
+    this.crud.getAll(this.deaprtmentsURL)
+      .subscribe(
+        res => {
+          this.departments = res;
+          console.log(res);
+        },
+        err => {
+          console.log(err);
+        })
+  }
+
+  createPhotoalbum(data) {
+    this.crud.create(this.baseURL, data)
+      .subscribe(
+        res => {
+          console.log(res);
+        }
+        , err => {
+          console.log(err);
+        })
   }
 
 }
